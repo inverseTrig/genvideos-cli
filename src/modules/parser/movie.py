3b5603c203f9db41ebf1473ddcb62a27d2c47cf7
HeeChan Kang <heech.kang@gmail.com>
2019-03-19 09:18:53 +0630

    Working roughdraft POC

--- src/modules/parser/movie.py
+++ src/modules/parser/movie.py
@@ -1,5 +1,3 @@
-SHOW_GENRE = False
-
 class Movie(object):
     def __init__(self, title, href, year, quality, rating, genre):
         self.title = title
@@ -10,6 +8,4 @@ class Movie(object):
         self.genre = genre
 
     def as_list(self):
-        if SHOW_GENRE:
-            return [self.title, self.year, self.quality, self.rating, self.genre]
-        return [self.title, self.year, self.quality, self.rating]
+        return [self.title, self.year, self.quality, self.rating, self.genre]
--- src/modules/parser/roughdraft.py
+++ src/modules/parser/roughdraft.py
@@ -8,9 +8,10 @@ from prettytable import PrettyTable
 from movie import Movie
 
 SHOW_GENRE = False
+SHOW_RATING = True
 
-# _searchparam = input('Search: ')
-_searchparam = 'spider'
+_searchparam = input('Search: ')
+# _searchparam = 'spider'
 r = requests.get('https://genvideos.co/results?q=' + _searchparam)
 
 parser = etree.HTMLParser()
@@ -37,14 +38,22 @@ for ele in _elements:
     _movies.append(Movie(_title, _href, _year, _quality, _rating, _genre))
 
 pt = PrettyTable()
-if SHOW_GENRE:
-    pt.field_names = ['ID', 'Title', 'Year', 'Quality', 'Rating', 'Genre']
-else:
-    pt.field_names = ['ID', 'Title', 'Year', 'Quality', 'Rating']
-number = [0]
-for movie in _movies:
-    pt.add_row([*number, *movie.as_list()])
+pt.field_names = ['ID', 'Title', 'Year', 'Quality', 'Rating', 'Genre']
 
-pt.reversesort = True
-pt.sortby = "Rating"
-print(pt)
+number = 1
+for movie in _movies:
+    pt.add_row([number, *movie.as_list()])
+    number += 1
+
+_fields = ['ID', 'Title', 'Year', 'Quality']
+pt.align['Title'] = 'l'
+
+if SHOW_RATING:
+    _fields.append('Rating')
+if SHOW_GENRE:
+    _fields.append('Genre')
+
+print(pt.get_string(fields=_fields))
+
+_userchoice = int(input('Enter ID: '))
+print(_movies[_userchoice-1].href)
